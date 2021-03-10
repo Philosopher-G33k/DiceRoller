b65cd2fff9bd85a0181a0a134fbaad5ce595cee6
Ishan Malviya <ishan.malviya@gmail.com>
2021-03-11 00:45:49 +0530

    Initial commit

---
+++ app/src/main/java/com/thebinarystudios/ishanmalviya/diceroller/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.thebinarystudios.ishanmalviya.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.roll_button)
+        button.text = "Let's Roll"
+        button.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+
+        val randomInteger = Random().nextInt(6) + 1
+        val diceImageView: ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when (randomInteger) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImageView.setImageResource(drawableResource)
+    }
+}
